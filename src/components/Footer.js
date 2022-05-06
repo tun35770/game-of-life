c7c337e7888c16702df780feb86584f7739164df
tun35770 <tun35770@temple.edu>
2022-05-06 19:16:28 -0400

    Fixed paths

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,12 @@ import { Link, useLocation } from 'react-router-dom'
 const Footer = () => {
   
   const location = useLocation()
-  
+  console.log(location.pathname)
   return (
     <>
       <footer>Ryan Murphy, Copyright &copy; 2022 </footer>
-      {location.pathname === '/game-of-life' && <Link to='/How-To-Play'><span>What is this?</span></Link>}
+      {(location.pathname === '/game-of-life/' || location.pathname === '/game-of-life')
+       && <Link to='/How-To-Play'><span>What is this?</span></Link>}
     </>
   )
 }
